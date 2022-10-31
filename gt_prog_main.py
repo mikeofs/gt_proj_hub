5e361d4099ddd8eb4f8dd85819659d7e1a6224f5
mikeofs <brmw@mail.ru>
2022-10-31 16:07:09 +0300

    patmatch

--- gt_prog_main.py
+++ gt_prog_main.py
@@ -3,3 +3,13 @@ from functools import reduce
 
 def line_raze(*args):
     return reduce(lambda res, nxt: nxt + res + nxt + ['newgit '], args)
+
+get_in = input()
+
+match get_in:
+    case [com , int(val)]:
+        val += 20
+        print(f"{com=}  ha {val=}")
+    case (a1, a2, a3):
+        print(a1, a2 , a3)
+
